91e5af3e5eeeeb71cc9e65ac2ed8bbf06c076097
Leo Liu <leoliu@microsoft.com>
2023-10-17 22:49:11 +0800

    build nav bar using antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout,Menu } from 'antd'
+import { Route, Routes, useNavigate} from 'react-router-dom';
+import { Home } from './home';
+import { Login } from './login';
+import { Register } from './register';
+const { Header,Content } = Layout;
+
+const getItem = (label:string, key:string, icon:any, children:any) => {
+  return {
+    label,
+    key,
+    icon,
+    children,
+  };
+}
+
+const items = [
+  getItem("Home", "/", null, null),
+  getItem("Register", "/register ", null, null),
+  getItem("Login", "/login", null, null),
+]
 
 function App() {
+  const navigate =useNavigate();
+
+  const onClick = (e:any) => {
+  navigate(e.key, { replace: true });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  
+    <Layout className="layout">
+      <Header style={{ display: 'flex', alignItems: 'center' }}>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          items={items}
+          onClick={onClick}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+        </Menu>
+      </Header>
+      <Content>
+        <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+      </Routes>    
+      </Content>
+    </Layout>  
+  
   );
-}
+};
 
 export default App;
