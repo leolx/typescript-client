c7ad0ac04d04a8b917e3aecaadce2f9329f7e0f0
Leo Liu <leoliu@microsoft.com>
2023-10-24 22:35:07 +0800

    add telephone

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,49 @@
 import React from 'react';
-import { Layout,Menu } from 'antd'
-import { Route, Routes, useNavigate} from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Telephone } from './telephone';
 import { Home } from './home';
 import { Login } from './login';
 import { Register } from './register';
-const { Header,Content } = Layout;
+const { Header, Content } = Layout;
 
-const getItem = (label:string, key:string, icon:any, children:any) => {
-  return {
-    label,
-    key,
-    icon,
-    children,
-  };
-}
+const getItem = (label: string, key: string, icon: any, children: any) => {
+	return {
+		label,
+		key,
+		icon,
+		children,
+	};
+};
 
 const items = [
-  getItem("Home", "/", null, null),
-  getItem("Register", "/register ", null, null),
-  getItem("Login", "/login", null, null),
-]
+	getItem('Home', '/', null, null),
+	getItem('Register', '/register ', null, null),
+	getItem('Login', '/login', null, null),
+	getItem('Telephone', '/telephone', null, null),
+];
 
 function App() {
-  const navigate =useNavigate();
+	const navigate = useNavigate();
 
-  const onClick = (e:any) => {
-  navigate(e.key, { replace: true });
-  }
-  return (
-  
-    <Layout className="layout">
-      <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          items={items}
-          onClick={onClick}
-        >
-   
-        </Menu>
-      </Header>
-      <Content>
-        <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/register' element={<Register/>}></Route>
-      </Routes>    
-      </Content>
-    </Layout>  
-  
-  );
-};
+	const onClick = (e: any) => {
+		navigate(e.key, { replace: true });
+	};
+	return (
+		<Layout className="layout">
+			<Header style={{ display: 'flex', alignItems: 'center' }}>
+				<Menu theme="dark" mode="horizontal" items={items} onClick={onClick}></Menu>
+			</Header>
+			<Content>
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/login" element={<Login />}></Route>
+					<Route path="/register" element={<Register />}></Route>
+					<Route path="/telephone" element={<Telephone />}></Route>
+				</Routes>
+			</Content>
+		</Layout>
+	);
+}
 
 export default App;
---
+++ src/telephone/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './index.module.less';
+
+export const Telephone = () => {
+	return (
+		<>
+			<div className={styles.page_top}>
+				<div className={styles.nav_bar}>
+					<div className={styles.logo}></div>
+					<div className={styles.site_bar}></div>
+				</div>
+				<div className={styles.banner}></div>
+			</div>
+		</>
+	);
+};
